ac41c86f519a4ec00633e774df3c81e3435fcf5a
Norman Lopez <noell.lpz@gmail.com>
2023-10-02 14:50:12 -0700

    removed unecessarry vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import checker from 'vite-plugin-checker';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-import builtins from "rollup-plugin-node-builtins";
-import polyfillNode from 'rollup-plugin-polyfill-node'
 import { VitePWA } from 'vite-plugin-pwa'
-import commonjs from '@rollup/plugin-commonjs';
-import svgr from "vite-plugin-svgr";
-import { fileURLToPath, URL } from 'node:url'
-import babel from 'vite-plugin-babel';
 
 let faviconURL = '/favicon.ico'
-
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
 
     const env = loadEnv(mode, process.cwd(), '')
 
-    const externals = ['end-of-stream', 'pump', 'lodash.merge', 'lodash', 'react', 'react/jsx-runtime', 'lodash.clonedeep', 'react-dom/client',
-        'borsh', 'bigint-buffer', 'rpc-websockets/dist/lib/client', '@zerodevapp/sdk', 'ably', 'react-dom', 'prop-types', 'phaser', 'lodash.isequal']
-
     return {
         base: './',
         plugins: [
-            // babel({
-            //     babelConfig: {
-            //         babelrc: false,
-            //         configFile: false,
-            //         plugins: ['@babel/plugin-transform-runtime']
-            //     }
-            // }),
-            // svgr(),
             react(),
-            // commonjs(),
             checker({
                 overlay: { initialIsOpen: false },
                 typescript: true,
@@ -45,8 +21,6 @@ export default defineConfig(({ command, mode }) => {
                     lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
                 },
             }),
-            // svgrPlugin(),
-            // polyfillNode(),
             VitePWA({
                 includeAssets: ['favicon.ico'],
                 manifest: {
@@ -75,7 +49,6 @@ export default defineConfig(({ command, mode }) => {
         define: {
             VITE_PUBLIC_URL: JSON.stringify(env.VITE_PUBLIC_URL),
             'process.env': {},
-            // "global": "global"
         },
         resolve: {
             alias: {
@@ -109,52 +82,12 @@ export default defineConfig(({ command, mode }) => {
                 buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
             }
         },
-        optimizeDeps: {
-            // include: ['end-of-stream'],
-            // exclude: externals,
-            // esbuildOptions: {
-            //     target: "es2020",
-            //     supported: { bigint: true },
-            //     plugins: [
-            //         NodeGlobalsPolyfillPlugin({
-            //             buffer: true,
-            //         }),
-            //         NodeModulesPolyfillPlugin(),
-            //     ],
-            // },
-        },
         build: {
             commonjsOptions: {
                 transformMixedEsModules: true
               },
-            minify: false,
-            sourcemap: true, // uncomment this line to debug source maps
-            // target: "es2020",
-            // rollupOptions: {
-            //     plugins: [
-            //         // Enable rollup polyfills plugin
-            //         // used during production bundling
-            //         // builtins(),
-            //         // rollupNodePolyFill(),
-            //         // commonjs(),
-            //     ],
-            //     // output: {
-            //     //     manualChunks: {
-            //     //         'scenes': ['src/scenes/SpinWheelScene.ts', 'src/scenes/AIGameScene.ts'],
-            //     //         'handlers': ['src/game-domain/GenerateQuestionsHandler.ts', /* other handlers */],
-            //     //         'utilities': ['src/utils/MessageListener.ts', /* other utilities */],
-            //     //         // 'phaser': ['phaser'],
-            //     //         '@zerodevapp': ['@zerodevapp/sdk'],
-            //     //         '@web3auth/solana-provider': ['@web3auth/solana-provider'],
-            //     //         '@web3auth/modal': ['@web3auth/modal'],
-            //     //         '@web3auth/base': ['@web3auth/base'],
-            //     //         // '@ethersproject': ['@ethersproject'],
-            //     //         // '@account-abstraction': ['@account-abstraction'],
-            //     //         // '@solana': ['solana'],
-            //     //         // 'assets': ['public/assets/Screens/in-game-bg-seagreen-1920x1080.png', 'public/assets/Screens/in-game-bg-green-1920x1080.png']
-            //     //     }
-            //     // }
-            // },
+            // minify: true,
+            // sourcemap: false, // uncomment this line to debug source maps
         },
 
     }
