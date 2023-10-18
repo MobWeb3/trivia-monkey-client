6a42360f548a443426cfd2e543c38d45e50ad0c5
Norman Lopez <noell.lpz@gmail.com>
2023-10-18 16:50:02 -0700

    chang prod-service scripts in package.json and ApiServiceConfig.ts

--- src/ApiServiceConfig.ts
+++ src/ApiServiceConfig.ts
@@ -2,6 +2,6 @@
 const DEV_BASE_URL = 'http://localhost:3333';
 const PROD_BASE_URL = 'https://monkey-trivia-server-e2c4b5238a63.herokuapp.com';
 
-const isProd = import.meta.env.MODE === 'prod-service' ? true : false;
+const isDev = import.meta.env.MODE === 'dev-service' ? true : false;
 
-export const BASE_URL = isProd ? PROD_BASE_URL : DEV_BASE_URL;
+export const BASE_URL = isDev ? DEV_BASE_URL : PROD_BASE_URL;
