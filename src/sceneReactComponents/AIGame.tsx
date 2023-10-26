083c89cd0a524ba66ffa82417851360061393852
Norman Lopez <noell.lpz@gmail.com>
2023-10-26 06:33:59 -0700

    extract out poll helpers

--- src/sceneReactComponents/AIGame.tsx
+++ src/sceneReactComponents/AIGame.tsx
@@ -14,6 +14,7 @@ import useLocalStorageState from 'use-local-storage-state';
 import { GameSession } from '../game-domain/GameSession';
 import { Wheel } from 'react-custom-roulette'
 import { WheelData } from 'react-custom-roulette/dist/components/Wheel/types';
+import { pollForCurrentPlayerId, pollUntilSessionChanges } from '../polybase/PollHelpers';
 
 function AIGame() {
 
@@ -205,66 +206,6 @@ function AIGame() {
     );
 }
 
-function pollForCurrentPlayerId(sessionId: string): Promise<any> {
-    let tries = 0; // Initialize tries counter
-    const maxTries = 3; // Set maximum number of tries
-    return new Promise(async (resolve, reject) => {
-        const intervalId = setInterval(async () => {
 
-            try {
-                console.log("polling for session id: ", sessionId);
-                // Get session data
-                const session = await getSession({ id: sessionId });
-                const { currentTurnPlayerId } = session;
-
-                // If currentTurnPlayerId is not null, clear the interval and resolve the Promise
-                if (currentTurnPlayerId) {
-                    clearInterval(intervalId);
-                    resolve(session);
-                }
-            } catch (error) {
-                console.log("error polling for current player id: ", error);
-            }
-
-
-            // If maximum number of tries has been reached, clear the interval and reject the Promise
-            if (++tries >= maxTries) {
-                clearInterval(intervalId);
-                reject('Timeout: Maximum number of tries reached');
-            }
-        }, 2000); // 1000 ms = 1 second
-    });
-}
-
-function pollUntilSessionChanges(expectedCurrentPlayerId: string, sessionId: string): Promise<GameSession> {
-
-    return new Promise(async (resolve, reject) => {
-        let tries = 0; // Initialize tries counter
-        const maxTries = 3; // Set maximum number of tries
-        const intervalId = setInterval(async () => {
-            // Get new session data
-            try {
-                const newSession = await getSession({ id: sessionId });
-                // if (!newSession) {
-                //     console.log('session not initialized yet');
-                //     this.messageGameText?.setText("session not initialized yet");
-                // }
-
-                if (expectedCurrentPlayerId === newSession.currentTurnPlayerId) {
-                    clearInterval(intervalId);
-                    resolve(newSession);
-                }
-            } catch (error) {
-                console.log("error polling for session changes: ", error);
-            }
-
-            // If maximum number of tries has been reached, clear the interval and reject the Promise
-            if (++tries >= maxTries) {
-                clearInterval(intervalId);
-                reject('Timeout: Maximum number of tries reached');
-            }
-        }, 2000); // 1000 ms = 1 second
-    });
-}
 
 export default AIGame;
