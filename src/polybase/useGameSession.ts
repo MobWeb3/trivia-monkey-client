f6ce56a599ea0e6e48f49108ec40199c0b3ad41e
Norman Lopez <noell.lpz@gmail.com>
2023-11-29 02:17:16 -0700

    Add useEffect hook to fetch session data

--- src/polybase/useGameSession.ts
+++ src/polybase/useGameSession.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getSession } from './SessionHandler';
 import useLocalStorageState from 'use-local-storage-state';
 import { SessionData } from '../screens/SessionData';
@@ -18,26 +18,32 @@ function useGameSession() {
     return !isEqual(before, after);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const ds = db.collection(COLLECTION_NAME).onSnapshot(
-    (newDoc) => {
-      async function fetchSession() {
-        const newSession = await getSession({ id: sessionData?.sessionId });
-        setSession(newSession);
+  useEffect(() => {
+    const collectionReference = db.collection(COLLECTION_NAME).onSnapshot(
+      (newDoc) => {
+        async function fetchSession() {
+          const newSession = await getSession({ id: sessionData?.sessionId });
+          setSession(newSession);
+        }
+  
+        console.log('newDoc', newDoc);
+        const new_session = newDoc.data[0].data;
+        // console.log('newSession', new_session);
+        if (hasChanged(session, new_session)) {
+          console.log('session changed: session data', sessionData);
+          fetchSession();
+        }
+      },
+      (err) => {
+        // Optional error handler
       }
-
-      console.log('newDoc', newDoc);
-      const new_session = newDoc.data[0].data;
-      // console.log('newSession', new_session);
-      if (hasChanged(session, new_session)) {
-        console.log('session changed: session data', sessionData);
-        fetchSession();
-      }
-    },
-    (err) => {
-      // Optional error handler
-    }
-  );
+    );
+  
+    return () => {
+      collectionReference();
+    };
+  }, [session, sessionData]); // Dependencies
+  
   return session;
 }
 
