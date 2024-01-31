54384d19d96b6ae39adecbff4915d9abf0ed48ff
Norman Lopez <noell.lpz@gmail.com>
2024-01-31 01:42:15 -0800

    Refactor generateAllQuestions function to simplify topic filtering and logging -Custom and General question generation is now decided on the service given the topic properties (metaphor_id vs general_id)

--- src/game-domain/GenerateQuestionsHandler.ts
+++ src/game-domain/GenerateQuestionsHandler.ts
@@ -1,40 +1,13 @@
-import axios from 'axios';
-import { BASE_URL } from '../ApiServiceConfig';
 import { requestQuestions } from '../mongo/QuestionHandler';
 import { Topic } from './Topic';
 
 export const generateAllQuestions = async (topics: Topic[]) => {
-    // from topics, create an array of topics that contain a metaphor id
-    const customTopics = topics.filter((topic) => {
-        const metaphor_id = topic.metaphor_id;
-        return metaphor_id ? metaphor_id.length > 0: false;
-    }).map((topic) => {
-        const topicName = topic.name;
-        const metaphor_id = topic.metaphor_id;
-        return { metaphor_id, name: topicName };
-    });
-
-    if (customTopics.length > 0) {
+    if (topics.length > 0) {
         try {
-            console.log("GENERATING CUSTOM QUESTIONS FOR TOPICS: ", customTopics);
-            requestQuestions(customTopics);
+            console.log("GENERATING QUESTIONS FOR TOPICS: ", topics);
+            requestQuestions(topics);
         } catch (error) {
             console.error(error);
         }
     }
-
-    topics.map(async (topic) => {
-        // If the topic has empty metaphor id, it is a general topic question
-        if (topic?.metaphor_id?.length === 0) {
-            try {
-                console.log("GENERATING GENERAL QUESTIONS FOR TOPIC: ", topic.name);
-                axios.post(
-                    `${BASE_URL}/api/openai/generalTopicQuestions`,
-                    { topic: topic.name }
-                );
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    });
 }
