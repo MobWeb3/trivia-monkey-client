e76d7b5627c9be71ae23c1dd720d22184a2e094d
Norman Lopez <noell.lpz@gmail.com>
2024-01-31 20:24:34 -0800

    Update BASE_URL and MENG_URL in ApiServiceConfig

--- src/ApiServiceConfig.ts
+++ src/ApiServiceConfig.ts
@@ -1,13 +1,12 @@
 // const DEV_BASE_URL = 'https://tough-shark-literally.ngrok-free.app';
 const DEV_BASE_URL = 'http://localhost:3333';
-const PROD_BASE_URL = 'https://monkey-trivia-server-e2c4b5238a63.herokuapp.com';
 
 export const isDev = import.meta.env.MODE === 'dev-service' ? true : false;
 
-export const BASE_URL = isDev ? DEV_BASE_URL : PROD_BASE_URL;
+export const BASE_URL = isDev ? DEV_BASE_URL : import.meta.env.VITE_PROD_BASE_URL;
 
 // export const BASE_AI_URL = 'https://74.82.30.101:5000'
 
 export const DEV_MENG_URL = 'http://127.0.0.1:8000'
 
-export const MENG_URL = isDev ? DEV_MENG_URL : process.env.VITE_PROD_MENG_URL;
+export const MENG_URL = isDev ? DEV_MENG_URL : import.meta.env.VITE_PROD_MENG_URL;
