7e7abc1c81b9c6ac2de49ac3caf00894a7ed8df4
Norman Lopez <noell.lpz@gmail.com>
2024-02-19 15:36:20 -0700

    Add Logout functionality and remove redundant code in PlayLobby.tsx

--- src/screens/PlayLobby.tsx
+++ src/screens/PlayLobby.tsx
@@ -3,12 +3,10 @@ import CustomButton from "../components/CustomButton";
 import { Flex, Image } from '@mantine/core';
 import './PlayLobby.css'
 import monkeyTriviaLogo from '../assets/Screens/signin/monkey-trivia-arched-name-400x200.png';
-import { useContext } from "react";
-import { SignerContext } from "../components/SignerContext";
+import { logout } from "../authentication/Logout";
 
 const PlayLobbyScene = () => {
     const navigate = useNavigate();
-    const { web3auth } = useContext(SignerContext);
 
     const handleCreateGameClick = () => {
         console.log('Create Game');
@@ -20,25 +18,6 @@ const PlayLobbyScene = () => {
         console.log('Go to profile page');
         // Navigate to CreateGame
         navigate('/profile');
-    };
-
-
-    const logout = async () => {
-        if (!web3auth) {
-            console.log('web3auth not initialized yet');
-            return;
-        }
-        try {
-            await web3auth.logout();
-            web3auth.clearCache();
-        } catch (error) {
-            console.error(error);
-        }
-        localStorage.removeItem('userInfo'); // Deprecated
-        localStorage.removeItem('web3auth'); // Deprecated
-        localStorage.removeItem('sessionData');
-        localStorage.removeItem('authSessionData');
-        console.log('Disconnected');
     };
 
     return (
